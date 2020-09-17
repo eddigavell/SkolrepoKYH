7f74285a373d9005e6e6964f6eab55db23fbb884
Andreas Berg <andreas.berg2@student.kyh.se>
2020-09-17 14:32:30 +0200

    made the catch smaller

--- src/com/company/RepeatWithErrors.java
+++ src/com/company/RepeatWithErrors.java
@@ -30,12 +30,13 @@ public class RepeatWithErrors {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        try {
+
             System.out.print("Write something: ");
             String input = sc.next();
+
             System.out.print("Number of times to repeat?: ");
             int nRepeat = sc.nextInt();
-
+        try {
             System.out.print(input.repeat(nRepeat));
         } catch (Exception e) {
             System.out.println("Error, cannot understand that number");
