9c91dcb145db204d478319895104f710ec8734ef
Edvin Gavell <eddi@live.se>
2020-09-18 11:18:17 +0200

    Added recursionb and functionSum

---
+++ src/com/company/lecture4/Recursion.java
@@ -0,0 +1,28 @@
+package com.company.lecture4;
+
+public class Recursion {
+    public static void main(String[] args) {
+        // Starta med att anropa funktionen
+        helloRec(5);
+    }
+
+    public static void helloRec(int i){
+        /*
+        helloRec: Denna funktion tar in ett heltal "i".
+
+        Instruktioner:
+
+        1: Om heltalet är 0 ska vi avsluta (Använd return) ("Basfall").
+
+        Annars:
+        2: Skriv ut "Hello " + i.
+        3: Anropa helloRec igen, men minska i med 1.
+        */
+
+        // När ska vi stanna? ("Basfall")
+
+        // Skriv ut: Hello i
+
+        // Anropa sig själv
+    }
+}
---
+++ src/com/company/lecture4/functionSum.java
@@ -0,0 +1,21 @@
+package com.company.lecture4;
+
+public class functionSum {
+
+    public static int sum(int i) {
+
+        if( i == 0) {
+            return 0;
+        } else {
+           return sum(i-1) + i;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        //Rekursion att kalla på en funktion att anropa sig själv.
+
+        System.out.println(sum(4));
+    }
+}
