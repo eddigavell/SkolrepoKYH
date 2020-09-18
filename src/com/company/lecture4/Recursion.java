2d32ea6e2805a4d8a5d7a5dd2739ce421ff3bb59
Edvin Gavell <eddi@live.se>
2020-09-18 11:25:09 +0200

    Updated recursion

--- src/com/company/lecture4/Recursion.java
+++ src/com/company/lecture4/Recursion.java
@@ -1,28 +1,31 @@
 package com.company.lecture4;
 
 public class Recursion {
-    public static void main(String[] args) {
-        // Starta med att anropa funktionen
-        helloRec(5);
-    }
 
     public static void helloRec(int i){
         /*
         helloRec: Denna funktion tar in ett heltal "i".
 
         Instruktioner:
+         */
 
-        1: Om heltalet är 0 ska vi avsluta (Använd return) ("Basfall").
 
-        Annars:
-        2: Skriv ut "Hello " + i.
-        3: Anropa helloRec igen, men minska i med 1.
-        */
+        if (i == 0) {
+            //1: Om heltalet är 0 ska vi avsluta (Använd return) ("Basfall").
+            return;
+        } else {
+        //Annars:
+        //2: Skriv ut "Hello " + i.
+        System.out.println("Hello " + i);
+        //3: Anropa helloRec igen, men minska i med 1.
+        helloRec(i-1);
+            // När ska vi stanna? ("Basfall") --> Kmr åka ur tack vare if i==0.
+        }
+    }
 
-        // När ska vi stanna? ("Basfall")
 
-        // Skriv ut: Hello i
-
-        // Anropa sig själv
+    public static void main(String[] args) {
+        // Starta med att anropa funktionen
+        helloRec(5);
     }
 }
