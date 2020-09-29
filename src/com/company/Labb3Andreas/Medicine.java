7a4904c4613aafa6e764ae87bb91b0e97648dd1e
Andreas Berg <andreas.berg2@student.kyh.se>
2020-09-29 09:05:06 +0200

    made my own lecture7

---
+++ src/com/company/Labb3Andreas/Medicine.java
@@ -0,0 +1,14 @@
+package com.company.Labb3Andreas;
+
+class Medicine {
+    private String treatsSickness;
+
+    public Medicine(String sickness) {
+        this.treatsSickness = sickness;
+    }
+
+    public String getTreatmentName() {
+        return treatsSickness;
+    }
+}
+
---
+++ src/com/company/lektion7Andreas/Shape.java
@@ -0,0 +1,4 @@
+package com.company.lektion7Andreas;
+
+public interface Shape {
+}
