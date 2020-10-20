f0161d990c3c9f13d13316ceba4180347bb0ed31
Edvin Gavell <eddi@live.se>
2020-10-20 17:11:40 +0200

    Updated code ovningstenta

--- src/OvningsTenta/PlayingCard.java
+++ src/OvningsTenta/PlayingCard.java
@@ -4,7 +4,7 @@ class PlayingCard {
     private int value;
     private String color;
 
-    public PlayingCard(int x, String y) {
+    PlayingCard(int x, String y) {
         try {
             setValue(x);
             setColor(y);
@@ -13,11 +13,11 @@ class PlayingCard {
         }
     }
 
-    public PlayingCard() {
+    PlayingCard() {
         this(1,"Hearts");
     }
 
-    public void setValue(int x) {
+    void setValue(int x) {
         if (x >= 1 && x <=13) {
             this.value = x;
         } else {
@@ -25,7 +25,7 @@ class PlayingCard {
         }
     }
 
-    public void setColor(String y) {
+    void setColor(String y) {
         if(y.equals("Hearts") || y.equals("Spades") || y.equals("Diamonds") || y.equals("Clubs")) {
             this.color = y;
         } else {
@@ -33,11 +33,11 @@ class PlayingCard {
         }
     }
 
-    public int getValue() {
+    int getValue() {
         return this.value;
     }
 
-    public String getColor() {
+    String getColor() {
         return this.color;
     }
 
@@ -45,22 +45,18 @@ class PlayingCard {
         return getValue()+" of "+getColor();
     }
 
-/* Den ska också ha en konstruktor som inte tar in något argument, den
-ska skapa upp en instans för kortet hjärter äss (“Hearts”, 1).
-*/
-
     public static void main(String[] args) {
         PlayingCard kort1 = new PlayingCard();
         //System.out.println(kort1);
 
 
-        //PlayingCard kort2 = new PlayingCard(5,"Diamonds");
+        PlayingCard kort2 = new PlayingCard(5,"Diamonds");
         //System.out.println(kort2);
 
-        //PlayingCard kort3 = new PlayingCard(-1, "Diamonds");
+        PlayingCard kort3 = new PlayingCard(-1, "Diamonds");
         //System.out.println(kort3);
 
-        //PlayingCard kort4 = new PlayingCard(4, "Fisk");
+        PlayingCard kort4 = new PlayingCard(4, "Fisk");
         //System.out.println(kort4);
 
         System.out.println(kort1.toString());
